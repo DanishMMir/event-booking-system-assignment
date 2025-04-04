acf23f0964abdad2964d28ebed99898e38561c71
DanishMMir <danish.majeed@keyloop.com>
2025-04-04 22:52:55 +0530

    + remove auth for routes

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\BookingController;
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+//Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('events', EventController::class)->except(['index', 'show']);
-});
+//});
 
 // Public routes
 Route::get('events', [EventController::class, 'index']);
